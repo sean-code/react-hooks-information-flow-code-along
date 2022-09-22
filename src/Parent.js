dcdc6844efd4f9a0a2a9087a5435e2ca0b4f91d6
sean-code <john.nganga@student.moringaschool.com>
2022-09-22 20:59:26 +0300

    Coded along

--- src/Parent.js
+++ src/Parent.js
@@ -6,6 +6,7 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  
   return (
     <div className="parent" style={{ backgroundColor: color }}>
       <Child />
